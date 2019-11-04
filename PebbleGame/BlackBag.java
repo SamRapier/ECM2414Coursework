aa64d66cdfbf240b6b85bdafbc29404a619af282
Sam Rapier <samrapier@live.co.uk>
2019-11-04 15:27:58 +0000

    High-level design

---
+++ PebbleGame/BlackBag.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class BlackBag here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BlackBag
+{
+    private int numPlayers;
+    private int numPebbles = 11 * numPlayers;
+    private int 
+}
